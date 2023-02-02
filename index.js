e5eee586f3eb5a01b8bffd0a1c2e8d328029065b
Kushagraved <kushagraved@gmail.com>
2023-02-02 20:22:00 +0530

    chore: add express for backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+app.get('/',async(req,res)=>{
+  res.json({
+    message:'API RUNNING !!'
+  });
+});
+
+
+const apiRoutes=require('./src/routes/apiRoutes');
+app.use('/api',apiRoutes);
+
+
+app.use((error,req,res,next)=>{
+  console.log(error);
+  next(error);
+});
+
+app.use((error,req,res,next)=>{
+  res.status(500).json({
+    message:error.message,
+    stack:error.stack
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
